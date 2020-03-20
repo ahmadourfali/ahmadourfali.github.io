fde94c65c35bfcaa57a2aa4a51064590554d8cd0
ahmadourfali <ahmedorfaly1982@gmail.com>
2020-03-20 10:27:22 +0200

    indext.html

--- myscript.js
+++ myscript.js
@@ -1,8 +1,12 @@
 
 count = 0;
-z = count % 3;
 function myfunction() {
-document.getElementById("counter").innerHTML = count;
-count += 1;
-if (z == 0) {document.getElementById("counter").style.color = "red";}
+  document.getElementById("counter").innerHTML = count;
+  count += 1;
+  z = count % 3;
+  if (z == 1) {
+    document.getElementById("counter").style.color = "red";
+  } else {
+    document.getElementById("counter").style.color ="black";
+  }
 }
