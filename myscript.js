24ba99e9bfcc359ca92a9008080adc6ed3c8a1a2
ahmadourfali <ahmedorfaly1982@gmail.com>
2020-03-23 09:52:02 +0200

    index.html

--- myscript.js
+++ myscript.js
@@ -1,12 +1,17 @@
 
 count = 0;
 function myfunction() {
-  document.getElementById("counter").innerHTML = count;
   count += 1;
   z = count % 3;
-  if (z == 1) {
-    document.getElementById("counter").style.color = "red";
-  } else {
-    document.getElementById("counter").style.color ="black";
-  }
+  y = count % 5;
+  document.getElementById("counter").innerHTML = count;
+  if (z == 0 && y == 0) {
+   document.getElementById("counter").style.color="green";
+ } else if (y == 0) {
+   document.getElementById("counter").style.color="red";
+ } else if (z == 0) {
+ document.getElementById("counter").style.color="blue";
+ } else {
+   document.getElementById("counter").style.color="black";
+ }
 }
