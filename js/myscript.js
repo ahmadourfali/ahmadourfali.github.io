65b338e8f0f627ee37ce273ae31663b008cf3b5d
ahmadourfali <ahmedorfaly1982@gmail.com>
2020-04-10 17:00:46 +0300

    flash cards

---
+++ js/myscript.js
@@ -0,0 +1,77 @@
+
+function replace1(){
+  if (document.getElementById("A").src="images/A.png") {
+      document.getElementById("A").src="images/studying.jpg";
+  }else if (document.getElementById("A").src="images/studying.jpg") {
+  document.getElementById("A").src="images/A.png.jpg"
+  }
+}
+function replace2() {
+  document.getElementById("B").src = "images/forgetting.jpg";
+}
+function replace3() {
+  document.getElementById("C").src = "images/giving.jpg";
+}
+function replace4() {
+  document.getElementById("D").src = "images/knowing.png";
+}
+function replace5() {
+  document.getElementById("E").src = "images/taking.jpg";
+}
+function replace6() {
+  document.getElementById("F").src = "images/asking.jpg";
+}
+function replace7() {
+  document.getElementById("G").src = "images/ordering.jpg";
+}
+function replace8() {
+  document.getElementById("H").src = "images/cooking.jpg";
+}
+function replace9() {
+  document.getElementById("I").src = "images/doing.jpg";
+}
+function replace10() {
+  document.getElementById("J").src = "images/writing.jpg";
+}
+function replace11() {
+  document.getElementById("K").src = "images/drinking.jpg";
+}
+function replace12() {
+  document.getElementById("L").src = "images/eating.jpg";
+}
+function replace13() {
+  document.getElementById("a").src = "images/drink.png";
+}
+function replace14() {
+  document.getElementById("b").src = "images/study.png";
+}
+function replace15() {
+  document.getElementById("c").src = "images/eat.png";
+}
+function replace16() {
+  document.getElementById("d").src = "images/write.png";
+}
+function replace17() {
+  document.getElementById("e").src = "images/know.png";
+}
+function replace18() {
+  document.getElementById("f").src = "images/order.png";
+}
+function replace19() {
+  document.getElementById("g").src = "images/forget.png";
+}
+function replace20() {
+  document.getElementById("h").src = "images/give.png";
+}
+function replace21() {
+  document.getElementById("i").src = "images/take.png";
+}
+function replace22() {
+  document.getElementById("j").src = "images/ask.png";
+}
+function replace23() {
+  document.getElementById("k").src = "images/do.png";
+}
+function replace24() {
+  document.getElementById("l").src = "images/cook.png";
+}
